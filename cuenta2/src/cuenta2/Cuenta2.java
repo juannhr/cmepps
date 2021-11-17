7c94365161269c7fc732b605540e30d0c27f5ead
juanh_hj7z1sj <juanh_hj7z1sj@LAPTOP-JFISRFD7>
2021-11-18 00:25:05 +0100

    Commit Inicial

---
+++ cuenta2/src/cuenta2/Cuenta2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuenta2;
+
+/**
+ *
+ * @author juanh_hj7z1sj
+ */
+public class Cuenta2 {
+
+ private int id, balance;
+
+
+    public Cuenta2(int id, int balance){
+
+        this.id = id;
+        this.balance = balance;
+
+    }
+
+
+   public void Withdraw(int x) throws Exception{
+
+
+
+
+   }
+
+   public void Deposit(int x){
+
+
+   }
+
+   public int getBalance(){
+       return 0;
+   }
+}
